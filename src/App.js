a4be99367304180a1d450c161c6fe8a6c236834f
AndrewLEEJM <woals214@gmail.com>
2020-12-17 18:04:32 +0900

    라이프사이클 테스트

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import { Component } from 'react';
 // import MyComponent from './MyComponent';
 import Event from './Event';
 import Validation from './Validation';
+import LifeCycle from './LifeCycle';
+
 import './App.css';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({ color: getRandomColor() });
+  };
+
   render() {
     const name = 'React Test';
     return (
@@ -14,6 +28,8 @@ class App extends Component {
         {/* <MyComponent favoriteNum={123}>Children이다</MyComponent> */}
         <Event />
         <Validation />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycle color={this.state.color} />
       </div>
     );
   }
